e72dbbfdf6e4d0e86333208e8946221b825c7a0c
cplan85 <carlos_planchart@live.com>
2022-08-21 13:04:50 +0200

    finished lazy loading

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,20 @@
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: '**', redirectTo: 'login'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
